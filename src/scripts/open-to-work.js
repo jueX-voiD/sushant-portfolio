85a7235ee1c897538a251009fe8518e2af601ea3
Jenish Shrestha <shrestha.jenish2000@gmail.com>
2025-07-28 10:17:55 +0545

    resume link added

--- src/scripts/open-to-work.js
+++ src/scripts/open-to-work.js
@@ -1,13 +1,31 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from(".open-to-work", {
-  y: 400,
-  opacity: 0,
-  duration: 1.2,
-  ease: "power4.out",
-  scrollTrigger: {
-    trigger: ".open-to-work",
-    start: "top 90%",
-    toggleActions: "play none none none",
+ScrollTrigger.matchMedia({
+  "(min-width: 641px)": function () {
+    gsap.from(".open-to-work", {
+      y: 400,
+      opacity: 0,
+      duration: 1,
+      ease: "power4.out",
+      scrollTrigger: {
+        trigger: ".open-to-work",
+        start: "top 92%",
+        toggleActions: "play none none none",
+      },
+    });
+  },
+
+  "(max-width: 640px)": function () {
+    gsap.from(".open-to-work", {
+      y: 400,
+      opacity: 0,
+      duration: 1,
+      ease: "power4.out",
+      scrollTrigger: {
+        trigger: ".mobile-open-to-work-trigger",
+        start: "top 60%",
+        toggleActions: "play none none none",
+      },
+    });
   },
 });
